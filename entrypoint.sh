a8542215183ff43476d2b0df2cf49cbaa5e0576d
Spenser Black <spenserblack01@gmail.com>
2021-11-05 07:53:13 -0400

    Output raw text

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 MSRV_RESULT=$(cargo msrv --bisect --path $1 --output-format json | tail -1)
-MSRV=$(echo $MSRV_RESULT | jq '.msrv')
+MSRV=$(echo $MSRV_RESULT | jq -r '.msrv')
 echo "::set-output name=msrv::$MSRV"
