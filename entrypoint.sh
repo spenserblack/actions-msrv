8e298a5406438ce8967914a426581071d390dacc
Spenser Black <spenserblack01@gmail.com>
2022-05-25 19:58:05 -0400

    Fix bad operators for `[`
    And I tested this out beforehand, too...

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@ MSRV_RESULT=$(cargo msrv --path $1 --output-format json | tail -1)
 MSRV=$(echo $MSRV_RESULT | jq -r '.msrv')
 echo "::set-output name=msrv::$MSRV"
 
-if [ "$2" == "true" ] || [ "$2" == "yes" ]; then
+if [ "$2" = "true" ] || [ "$2" = "yes" ]; then
 	cargo msrv set "$MSRV";
 fi
