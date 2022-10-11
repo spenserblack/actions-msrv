72016f3f05e70546940b5f616b52ae35d1fed073
Mark <79023646+con-mark@users.noreply.github.com>
2022-10-11 20:12:56 +0100

    #14 ::set-output is deprecated (#15)
    Co-authored-by: Spenser Black <spenserblack01@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 MSRV_RESULT=$(cargo msrv --path $1 --output-format json | tail -1)
 MSRV=$(echo $MSRV_RESULT | jq -r '.msrv')
-echo "::set-output name=msrv::$MSRV"
+echo "msrv=$MSRV" >> $GITHUB_OUTPUT
 
 if [ "$2" = "true" ] || [ "$2" = "yes" ]; then
 	cargo msrv set "$MSRV";
