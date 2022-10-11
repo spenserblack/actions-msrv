53057229602b9831c286741fc39a6f80fea778b4
Spenser Black <spenserblack01@gmail.com>
2022-10-11 15:11:13 -0400

    Remove placeholder braces

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 MSRV_RESULT=$(cargo msrv --path $1 --output-format json | tail -1)
 MSRV=$(echo $MSRV_RESULT | jq -r '.msrv')
-echo "{msrv}={$MSRV}" >> $GITHUB_OUTPUT
+echo "msrv=$MSRV" >> $GITHUB_OUTPUT
 
 if [ "$2" = "true" ] || [ "$2" = "yes" ]; then
 	cargo msrv set "$MSRV";
